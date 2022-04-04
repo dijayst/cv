3561816b52ef5b4f373c54347a4a0ed858233811
dijayst <dijayst@gmail.com>
2022-04-05 00:55:55 +0100

    update

--- src/Cv.js
+++ src/Cv.js
@@ -7,7 +7,7 @@ import {Slider} from '@material-ui/core';
 const useStyles=makeStyles({
     typo:{
         fontFamily:"Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
-        fontSize:"large",
+        fontSize:"small",
         marginRight:"18"
         
     },
@@ -16,7 +16,7 @@ const useStyles=makeStyles({
     },
     span:{
         color:"gold",
-        fontSize:"large"
+        fontSize:"small"
     },
     paper2:{
         float:"right",
@@ -79,149 +79,100 @@ const cust=[
 
    // const [val, setval] = useState([20])
    // const update=(e,data)=>{ setval(data) }
-   const val=20;
+   const html=100;
+     const sass=90; 
+      const mat=100; 
+       const spar=20; 
+        const uni=20; 
+         const reactjs=100; 
+          const ren=20; 
+           const js=100;
+           const blend=30;
     const classes=useStyles()
     return (
         <>
-        
-         <Paper className={classes.paper1}>
-             <Typography  variant="h5">STER</Typography>
-             <Typography variant="h6">Software Developer</Typography>
-             <Typography variant="p">Skills</Typography>
-             <hr className={classes.hr}/>
-             <div className={classes.skill}>
-             <Typography className={classes.typo} variant="body2">MySql </Typography>     
-            <Typography className={classes.typo} variant="body2">Mongo DB </Typography>
-             <Typography className={classes.typo} variant="body2">Git hub</Typography>
-             <Typography className={classes.typo} variant="body2">Javascript es6</Typography>
-             <Typography className={classes.typo} variant="body2">React JS</Typography>
-             <Typography className={classes.typo} variant="body2">Material UI </Typography>
-             
-             <Typography className={classes.typo} variant="body2"> Node js </Typography>
-             <Typography className={classes.typo} variant="body2"> HTML5 </Typography>
-             <Typography  className={classes.typo} variant="body2"> Css 3 </Typography>
-             <Typography className={classes.typo} variant="body2">Sass </Typography>
-             <Typography className={classes.typo} variant="body2"> Blender </Typography>
-             <Typography className={classes.typo} variant="body2"> Unity </Typography>
-              <Typography className={classes.typo} variant="body2"> Spark Ar</Typography>
-              </div>
-<br/>
-             <Typography variant="h5">EXPERIENCE</Typography>
-             <hr/>
-
-
-             <Typography variant="subtitle">skill</Typography>
-
-             <Typography variant="caption">yhello</Typography>
-             <Typography variant="overline">why</Typography>
-             <hr/>
-             <Typography variant="subtitle">skill</Typography>
-<br/>
-             <Typography variant="h5"> PROGRAMMING PROJECTS</Typography>
-             <hr/>
-             <div>
-                 
-             <Typography variant="h6">Tictacto</Typography>
-             <Typography varient="body2">it a game with two player where each take turns in playing. which ever player plays an horitontal or vertical game wins</Typography>
-             </div>
-            
-             <div>
-             <Typography variant="h6">grade calculator </Typography>
-             <Typography varient="body2"> A grade calculator which allows user to enter their score and it grades them accordingly.</Typography>
-             </div>
-             <div>
-             <Typography variant="h6"> Traffic light</Typography>
-          
-             <Typography varient="body2">it was inspired by the real life traffic light which switch  from red to yellow then green.</Typography>
-          
-             </div>
-             <div>
-             <Typography variant="h6"> Animated a female model using (blender)</Typography>
-          
-             </div>
-
-             <div>
-             <Typography variant="h6"> electrical lab vr</Typography>
-          
-             <Typography varient="body2">it was made using blender and unity  in an electrical lab which allows user to be abel to pick up equipment</Typography>
-          
-             </div>
-             <div>
-             <Typography variant="h6"> map puzzel</Typography>
-          
-             <Typography varient="body2">it made specially for kids to enable them know the names of the countries in the world.</Typography>
-          
-             </div><div>
-             <Typography variant="h6"> Weather App</Typography>
-          
-             <Typography varient="body2">here we fetched an api from open weather using  axios and react .</Typography>
-          
-             </div>
-             <div>
-             <Typography variant="h6"> Todo list</Typography>
-          
-             <Typography varient="body2">it helps users keep track of there activities.</Typography>
-          
-             </div>
-                </Paper>   
-
-
-
+<Paper className={classes.paper1} style={{backgroundColor:"#fff"}}>
+<div>
+                     
+                     <Typography  variant="h5">STER</Typography>
+                     <Typography variant="h6">Software Developer</Typography>
+                     <Typography variant="p">Skills</Typography>
+                     <hr className={classes.hr}/>
+                     <div className={classes.skill}>
+                     <Typography className={classes.typo} variant="body2">MySql </Typography>     
+                    <Typography className={classes.typo} variant="body2">Mongo DB </Typography>
+                     <Typography className={classes.typo} variant="body2">Git hub</Typography>
+                     <Typography className={classes.typo} variant="body2">Javascript es6</Typography>
+                     <Typography className={classes.typo} variant="body2">React JS</Typography>
+                     <Typography className={classes.typo} variant="body2">Material UI </Typography>
+                     </div>
+                     <div className={classes.skill}>
+                     <Typography className={classes.typo} variant="body2"> Node js </Typography>
+                     <Typography className={classes.typo} variant="body2"> HTML5 </Typography>
+                     <Typography  className={classes.typo} variant="body2"> Css 3 </Typography>
+                     <Typography className={classes.typo} variant="body2">Sass </Typography>
+                     <Typography className={classes.typo} variant="body2"> Blender </Typography>
+                     <Typography className={classes.typo} variant="body2"> Unity </Typography>
+                      <Typography className={classes.typo} variant="body2"> Spark Ar</Typography>
+                      </div>
+        <br/>
+                     <Typography variant="h5">EXPERIENCE</Typography>
+                     <hr/>
+                         </div>
+                     <Typography variant="h5">PROGRAMMING</Typography>
+                     <hr/> <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>React js</span>
+                     <Slider className={classes.Slider} value={reactjs}  marks={cust}/>
+                   
+                     </div> 
+                    
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>Javascript</span>
+                     <Slider className={classes.Slider} value={js}  marks={cust}/>
+                     </div> 
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>HTML & css</span>
+                     <Slider className={classes.Slider} value={html}  marks={cust}/>
+                   
+                     </div> 
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>Sass</span>
+                     <Slider className={classes.Slider} value={sass}  marks={cust}/>
+                   
+                     </div> 
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>React native</span>
+                     <Slider className={classes.Slider} value={ren}  marks={cust}/>
+                   
+                     </div> 
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>Material ui</span>
+                     <Slider className={classes.Slider} value={mat}  marks={cust}/>
+                   
+                     </div> 
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>Spark AR</span>
+                     <Slider className={classes.Slider} value={spar}  marks={cust}/>
+                   
+                     </div> 
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>unity</span>
+                     <Slider className={classes.Slider} value={uni}  marks={cust}/>
+                   
+                     </div> 
+                     <div style={{width:300,margin:60}}>
+                    <span className={classes.span}>Blender</span>
+                     <Slider className={classes.Slider} value={blend}  marks={cust}/>
+                   
+                     </div> 
+                    
+</Paper>
 
              <Paper className={classes.paper2}>
-                 
-             <Typography variant="h4">PROGRAMMING</Typography>
-             <hr/> <div style={{width:300,margin:60}}>
-            <span className={classes.span}>React js</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-            
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>Javascript</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>HTML & css</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>Sass</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>React native</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>Material ui</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>Spark AR</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>unity</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
-             <div style={{width:300,margin:60}}>
-            <span className={classes.span}>Blender</span>
-             <Slider className={classes.Slider} value={val}  marks={cust}/>
-           
-             </div> 
+                
             
             
-            
-              <Typography variant="h4">STRENGHTS</Typography>
+              <Typography variant="h5">STRENGHTS</Typography>
              <hr/>
               
              <Typography variant="body2"><Chat/>Oral and Written Communication</Typography>
@@ -233,7 +184,7 @@ const cust=[
              <Typography variant="body2">Team Building</Typography>
 
              
-             <Typography variant="h4">EDCATION</Typography>
+             <Typography variant="h5">EDUCATION</Typography>
              <hr/>
              
              <Typography variant="body2">post graduate of Software Engineering</Typography>
@@ -242,27 +193,50 @@ const cust=[
              
              <Typography variant="body2">GPA </Typography>
 
-             
-             <Typography variant="h4">PROGRAMMING PROJECTS</Typography>
+
+
+             <Typography variant="h5"> PROGRAMMING PROJECTS</Typography>
              <hr/>
+             <div >
+                 
+             <Typography variant="h6" style={{textDecoration:"underline"}}>*Tictacto</Typography>
+             <Typography varient="body2" className={classes.typo}>it a game with two player where each take turns in playing. which ever player plays an horitontal or vertical game wins</Typography>
              
-             <Typography variant="body2">Tictacto Game</Typography>
-             <hr/>
+             <Typography variant="h6" style={{textDecoration:"underline"}}>*grade calculator </Typography>
+             <Typography varient="body2" className={classes.typo}> A grade calculator which allows user to enter their score and it grades them accordingly.</Typography>
+             <Typography variant="h6" style={{textDecoration:"underline"}}>* Traffic light</Typography>
+          
+             <Typography varient="body2" className={classes.typo}>it was inspired by the real life traffic light which switch  from red to yellow then green.</Typography>
+          
+             <Typography variant="h6" style={{textDecoration:"underline"}}>* Animated a female model using (blender)</Typography>
+          
+             <Typography variant="h6" style={{textDecoration:"underline"}}> *electrical lab vr</Typography>
+          
+             <Typography varient="body2" className={classes.typo}>it was made using blender and unity  in an electrical lab which allows user to be abel to pick up equipment</Typography>
+          
+            
+             <Typography variant="h6" style={{textDecoration:"underline"}}>* map puzzel</Typography>
+          
+             <Typography varient="body2" className={classes.typo}>it made specially for kids to enable them know the names of the countries in the world.</Typography>
+          
              
-             <Typography variant="body2">face filter</Typography>
-             <hr/>
+             <Typography variant="h6" style={{textDecoration:"underline"}}>* Weather App</Typography>
+          
+             <Typography varient="body2" className={classes.typo}>here we fetched an api from open weather using  axios and react .</Typography>
+          
+             </div>
+             <div>
+             <Typography variant="h6" style={{textDecoration:"underline"}}>*Todo list</Typography>
+          
+             <Typography varient="body2" className={classes.typo}>it helps users keep track of there activities.</Typography>
+          
              
-             <Typography variant="body2">Ar game using spark</Typography>
-             <hr/>
+             <Typography variant="h6" style={{textDecoration:"underline"}}>*face filter</Typography>
              
+             <Typography variant="h6" style={{textDecoration:"underline"}}>*Ar game using spark</Typography>
              
+             </div>
              
-
-
-
-
-
-
              </Paper>
         </>
     )
