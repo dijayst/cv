d135f4fa77ac2fc9af0457d2eb70e1d086623bfd
dijayst <dijayst@gmail.com>
2022-04-04 01:43:12 +0100

    first commit

---
+++ src/Cv.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Paper, Typography} from '@material-ui/core'
+const Cv = () => {
+    return (
+        <>
+         <Paper>
+             <Typography variant="h1">STER</Typography>
+             <Typography variant="h5">Software Developer</Typography>
+             <Typography variant="h4">Skills</Typography>
+             <hr/>
+             <Typography variant="body2">MySql</Typography>     
+            <Typography variant="body2">MongoDB</Typography>
+             <Typography variant="body2">Github</Typography>
+             <Typography variant="body2">Javascript es6</Typography>
+             <Typography variant="body2">React JS</Typography>
+             <Typography variant="body2">Material UI</Typography>
+             <Typography variant="body2">Node</Typography>
+             <Typography variant="body2">HTML</Typography>
+             <Typography variant="body2">Css</Typography>
+             <Typography variant="body2">Sass</Typography>
+             <Typography variant="body2">Blender</Typography>
+             <Typography variant="body2">Unity</Typography>
+             <Typography variant="body2">Spark Ar</Typography>
+
+             <Typography variant="h4">EXPERIENCE</Typography>
+             <hr/>
+
+
+             <Typography variant="subtitle">skill</Typography>
+
+             <Typography variant="caption">yhello</Typography>
+             <Typography variant="overline">why</Typography>
+             <hr/>
+             <Typography variant="subtitle">skill</Typography>
+             </Paper>   
+             <Paper>
+                 
+             <Typography variant="h4">PROGRAMMING</Typography>
+             <hr/>
+
+             <Typography variant="h4">STRENGHTS</Typography>
+             <hr/>
+              
+             <Typography variant="body2">Oral and Written Communication</Typography>
+             
+             <Typography variant="body2">Reliable and Consistent</Typography>
+             
+             <Typography variant="body2">Committed to lifelong Learning</Typography>
+             
+             <Typography variant="body2">Team Building</Typography>
+
+             
+             <Typography variant="h4">EDCATION</Typography>
+             <hr/>
+             
+             <Typography variant="body2">post graduate of Software Engineering</Typography>
+             
+             <Typography variant="body2">Studied at NIIT</Typography>
+             
+             <Typography variant="body2">GPA </Typography>
+
+             
+             <Typography variant="h4">PROGRAMMING PROJECTS</Typography>
+             <hr/>
+             
+             <Typography variant="body2">Tictacto Game</Typography>
+             <hr/>
+             
+             <Typography variant="body2">face filter</Typography>
+             <hr/>
+             
+             <Typography variant="body2">Ar game using spark</Typography>
+             <hr/>
+             
+             
+             
+
+
+
+
+
+
+             </Paper>
+        </>
+    )
+}
+
+export default Cv
